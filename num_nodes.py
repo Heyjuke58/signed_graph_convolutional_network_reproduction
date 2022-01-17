3d91997011c902f9531328ee0b0f2bc678cc13c5
Konrad Wienecke <konrad.wienecke@mac.com>
2022-01-17 13:08:12 +0100

    cool script

---
+++ num_nodes.py
@@ -0,0 +1,10 @@
+with open("soc-sign-bitcoinalpha.csv") as file:
+    input = file.read()
+
+links = input.splitlines()
+nodes = set()
+for link in links:
+    source, target, _, _ = link.split(",")
+    nodes.add(source)
+    nodes.add(target)
+print(len(nodes))
